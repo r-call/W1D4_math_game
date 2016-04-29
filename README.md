d77e56cafe7969e9c73a89d62f8c6235c898be7e
Ryan Callaway <callaway.r@gmail.com>
2016-04-29 16:50:29 -0700

    fully functional math_game.rb

--- README.md
+++ README.md
@@ -1,7 +1,28 @@
 #pseudocode for two player math game
 
-This is a test of the pull feature
+math_game
+  initialize p1_lives, p2_lives = 3
+  player_turn = random (1 or 2)
 
-Test to see if setting up branch tracking worked
+  while p1_lives > 0 && p2_lives > 0
+    generate_question
+    player_ans = gets.chomp
+    subtract a life if player_ans is incorrect
+    display player_1, player_2 lives remaining
+  end
 
-Does sublime automatically open pulls?
+  announce winner and loser
+
+  play again?
+
+end
+
+generate_question
+  @answer
+
+  num_1 = 1 + rand(100)
+  num_2 = 1 + rand(100)
+
+  puts "what is #{num_1} + {num_2}?"
+  
+end
--- math_game.rb
+++ math_game.rb
@@ -1,1 +1,53 @@
-#2 player math game
+#2 player math game #pseudocode for two player math game
+
+require 'pry'
+require 'pry-byebug'
+
+def math_game
+  p1_lives = 3
+  p2_lives = 3
+  #player_turn = 1+ random (1)
+
+  while p1_lives > 0 && p2_lives > 0
+    puts "Player 1 go:"
+    generate_question
+    puts @answer
+    player_ans = Integer(gets.chomp)
+    binding.pry
+    if player_ans != @answer
+      p1_lives -= 1
+    end
+    
+    puts "Player 2 go:"
+    generate_question
+    puts @answer
+    player_ans = Integer(gets.chomp)
+    if player_ans != @answer
+      p2_lives -= 1
+    end
+
+    puts "Player 1: #{p1_lives} lives remaining   Player 2: #{p2_lives} lives remaining"
+  end
+
+  if (p1_lives ==0 && p2_lives ==0)
+    puts "You both suck"
+  elsif p1_lives == 0
+    puts "Player 1 loses"
+  else
+    puts "Player 2 loses"
+  end
+end
+
+def generate_question
+  @answer = 0
+
+  num_1 = 1 + rand(100)
+  num_2 = 1 + rand(100)
+
+  @answer = num_1 + num_2
+
+  puts "what is #{num_1} + #{num_2}?"
+  
+end
+
+math_game
